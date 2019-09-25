ae80db979f10a9d4bcd34b0b1b097fbc983f43f2
mesele16 <55573093+mesele16@users.noreply.github.com>
2019-09-25 09:59:04 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,7 +17,7 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
-PM_START_TEXT = """
+PM_KBRA_TEXT = """
 ሰላም {}, ስሜ {}!  ይባላል ያልገባዎት ነገር ካለ  /help ይጫኑ... 
 
 እኔ የ Group ኣለቃ ነኝ እኔን የሰራኝ ኣለቃየ ደሞ   [ይሄ ነው](t.me/mesatechs).
